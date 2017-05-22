339af96c59ece64953ab589a4de0d0f77d72d893
Meghana Singh <meghana1602@gmail.com>
2017-05-22 17:38:03 +0000

    Added the auto detele functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
     def show
       @user  = current_user
       @items = @user.items
+      Item.where("created_at <= ?", Time.now - 7.days).destroy_all
     end
 end
