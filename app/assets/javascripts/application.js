272a93e3047748899f5946d55627672817c8cf3f
Meghana Singh <meghana1602@gmail.com>
2017-06-06 19:44:09 +0000

    Added API call to webAnalytics for tracking number of user logins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,7 @@ webAnalytics.report = function(eventName) {
    var event   = {event: { name: eventName }};
    var request = new XMLHttpRequest();
  
-   request.open("POST", "http://localhost:3000/api/events", true);
+   request.open("POST", "https://web-analytics-meghana1602.c9users.io/api/events", true);
    request.setRequestHeader('Content-Type', 'application/json');
    request.send(JSON.stringify(event));
 };
